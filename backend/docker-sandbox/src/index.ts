89d1c718e583b65b9270162616c156ff7af70a7c
thaminuZs <th.sonal@hotmail.com>
2026-07-21 15:01:40 +0530

    bugfix: ts config

--- backend/docker-sandbox/src/index.ts
+++ backend/docker-sandbox/src/index.ts
@@ -1,22 +1,4 @@
-import { runJavaCode } from './java-runner/java.runner.ts';
-import { runCppCode } from './cpp-runner/cpp.runner.ts';
-import { runJSCode } from './js-runner/js.runner.ts';
+import { runJavaCode } from './java-runner/java.runner';
+import { runCppCode } from './cpp-runner/cpp.runner';
+import { runJSCode } from './js-runner/js.runner';
 
-const javaCode = `
-public class Fib {
-    public static void main(String[] args) {
-        int n = 10;
-        long a = 0, b = 1;
-        for (int i = 0; i < n; i++) {
-            System.out.println(a);
-            long tmp = a + b;
-            a = b;
-            b = tmp;
-        }
-        for (int i=0;;) { }
-    }
-}
-`;
-
-const res = runJavaCode('my_jre', 'fib.java', javaCode);
-console.log(res);
